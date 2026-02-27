5757d3628bec53ef7e6b1ec66448509e5c8ca8b8
Ryan Peck <ryanpeck@MacBookPro.lan>
2026-02-26 18:57:41 -0800

    attacker fix

--- java/src/shredders/Attacker.java
+++ java/src/shredders/Attacker.java
@@ -9,51 +9,60 @@ public class Attacker extends BabyRat {
     }
 
     public void doAction() throws GameActionException {
-        // search for cheese
+        // search for attackers
         RobotInfo[] nearbyInfos = rc.senseNearbyRobots();
 
         MapLocation enemyLoc = null;
         for (RobotInfo info : nearbyInfos) {
             if (info.getTeam() != rc.getTeam() && (info.getType().isCatType() || info.getType().isRatKingType())) {
                 enemyLoc = info.getLocation();  
-                Direction toEnemy = rc.getLocation().directionTo(enemyLoc);              
-                if (rc.canTurn(toEnemy)) {
-                    rc.turn(toEnemy);
                     break; 
                 }
             } 
+            if (enemyLoc != null) {
+            Direction toEnemy = rc.getLocation().directionTo(enemyLoc);              
+            if (rc.canTurn(toEnemy)) {
+                rc.turn(toEnemy);
+            }
             if (enemyLoc != null && rc.canAttack(enemyLoc)) {
                 rc.attack(enemyLoc);
                 rc.setIndicatorString("Attacking!");
+                return;
         }
     }
-
         MapLocation nextLoc = rc.adjacentLocation(rc.getDirection());
         if (rc.canRemoveDirt(nextLoc)) {
             rc.removeDirt(nextLoc);
+            return;
         }
+        if (enemyLoc != null) {  
+        Direction toEnemy = rc.getLocation().directionTo(enemyLoc);     
+        if (rc.canMove(toEnemy)) {
+            rc.move(toEnemy);
+            rc.setIndicatorString("Finding enemyLoc.");
+            return;
+        } else {
+            Direction left = toEnemy.rotateLeft();
+            Direction right = toEnemy.rotateRight();
+            if (rc.canMove(left)) {
+                rc.move(left);
+                return; 
+            }
+            if (rc.canMove(right))  {  
+                rc.move(right);
+                return;
+            }
+        }
+    }
 
         if (rc.canMoveForward()) {
             rc.moveForward();
-            rc.setIndicatorString("Finding cat.");
-        } else {
-            Direction left = rc.getDirection().rotateLeft();
-            Direction right = rc.getDirection().rotateRight();
-            if (rc.canMove(right)) {
-                rc.move(right);
-            } else if (rc.canMove(left)) {
-                rc.move(left);
-            } else {
-                d = directions[rand.nextInt(directions.length - 1)];
-                if (rc.canTurn(d)) 
+         } else {
+                d = directions[rand.nextInt(directions.length)];
+                if (rc.canTurn(d)) {
                     rc. turn(d);
+                }
             }
         }
+    }  
 
-        if ((enemyLoc != null) && rc.canAttack(enemyLoc)) {
-            rc.attack(enemyLoc);
-            rc.setIndicatorString("Victory or death!");
-        }
-
-    }    
-}
